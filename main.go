dc29d4026bf901d6b3734fce4a0cd9f8f1fe064a
venkataramana109 <pogirivenkataramana109@gmail.com>
2023-08-13 12:05:55 +0530

    function for add two numbers

--- main.go
+++ main.go
@@ -9,3 +9,9 @@ func main()  {
 	z := i + j
 	fmt.Println("the value of z is :=",z)
 }
+// Adding two numbers
+func AddTwoNumbers(n1 ,n2 int){
+	var n3 int
+	n3 = n1 +n2
+	fmt.Println("value after adding two numbers :", n3)
+}
